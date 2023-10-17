8a1f32f0f509706ca44d3a79f0f692d2f74a1bc0
mdnazmul42726 <nazmulofficial@outlook.com>
2023-10-18 01:56:14 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEOmjZYuBgnzltzIw5RZGYjlTUBrLOtn8",
+  authDomain: "meem-146ca.firebaseapp.com",
+  projectId: "meem-146ca",
+  storageBucket: "meem-146ca.appspot.com",
+  messagingSenderId: "1087969696318",
+  appId: "1:1087969696318:web:7c47d2fb00f1b018508f6f"
+};
+
+const app = initializeApp(firebaseConfig);
