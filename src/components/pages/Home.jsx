f53e00e5a365f46283232ddc49a21a7abd23bdf7
mdnazmul42726 <nazmulofficial@outlook.com>
2023-10-18 06:20:56 +0600

    Check out our hot products section added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -42,14 +42,14 @@ const Home = () => {
             {/* upcoming offer section start */}
          
             <div className="relative overflow-hidden bg-white mt-44 mx-5">
+                {/* <h1 className="text-3xl text-center">  features product</h1> */}
           <div className="mb-10">
-          <hr />
           </div>
                 <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
                     <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                         <div className="sm:max-w-lg">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Check out our hot products</h1>
-                            <p className="mt-4 text-xl text-gray-500">This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.</p>
+                            <p className="mt-4 text-xl text-gray-500">Buy products from the most reputed companies at the lowest prices!</p>
                         </div>
                         <div>
                             <div className="mt-10">
